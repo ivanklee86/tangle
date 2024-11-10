f463adcd80441d12c94e6e940b251f9611a90d7c
Ivan Lee <ivanklee86@gmail.com>
2024-11-10 08:33:26 -0500

    Project boostrapping  (#4)
    * Dockerfile and README updates.
    
    * Bootstrap project and structure.
    
    * An explantion.

---
+++ cmd/argocd-tangle-server/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServer(t *testing.T) {
+	t.Run("Basic test.", func(t *testing.T) {
+		assert.True(t, true)
+	})
+}
