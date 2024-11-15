7d8e34521e123d1b5c8bbc9f3b56c304b897a796
Ivan Lee <ivanklee86@gmail.com>
2024-11-15 10:11:16 +0000

    Graceful server shutdown.

--- cmd/argocd-tangle-server/main.go
+++ cmd/argocd-tangle-server/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
@@ -25,5 +28,21 @@ func New() *fiber.App {
 
 func main() {
 	app := New()
-	log.Fatal(app.Listen(":8081"))
+
+	// Set up graceful service shutdown.
+	gracefulShutdown := make(chan os.Signal, 1)
+	signal.Notify(gracefulShutdown, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	
+	go func() {
+		signal := <- gracefulShutdown
+		log.Fatalf("Shutting down.  Signal: %s", signal)
+		_ = app.Shutdown()
+	}()
+
+	// Start service.
+	if err := app.Listen(":8081"); err != nil {
+		log.Error(err)
+	}
+
+	// Cleanup tasks.
 }
