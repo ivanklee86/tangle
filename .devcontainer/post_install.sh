7bfb26b887ff573ff42d9a519760e1d4ec482686
Ivan Lee <ivanklee86@gmail.com>
2024-11-16 09:49:58 +0000

    Add pre-commit.

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+# Set up pre-commit
+pip install -r requirements.txt
+pre-commit install
+
 # Configure git
 if [ "$CODESPACES" != "true" ]; then
     echo "Running locally, configure gpg."
