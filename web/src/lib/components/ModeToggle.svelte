28c0ae179af07fc5bc56e8efb7db65d24ce1af2f
Ivan Lee <ivanklee86@gmail.com>
2024-12-31 19:44:34 +0000

    Add shared component.

---
+++ web/src/lib/components/ModeToggle.svelte
@@ -0,0 +1,30 @@
+<script>
+	// @ts-nocheck
+	import {
+		Col,
+		Container,
+		Row,
+		Button,
+		ThemeToggler,
+		Icon,
+		useColorMode
+	} from '@sveltestrap/sveltestrap';
+
+	useColorMode('dark');
+</script>
+
+<Container>
+	<Row>
+		<Col align="center">
+			<ThemeToggler let:currentColorMode let:toggleColorMode>
+				<Button color="primary" on:click={() => toggleColorMode()}>
+					{#if currentColorMode === 'dark'}
+						<Icon name="sun" />
+					{:else}
+						<Icon name="moon-stars-fill" />
+					{/if}
+				</Button>
+			</ThemeToggler>
+		</Col>
+	</Row>
+</Container>
