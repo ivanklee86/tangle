73b5831c06d9f92e8735634ee0361e53ea25d5c0
Ivan Lee <ivanklee86@gmail.com>
2025-01-12 10:15:56 +0000

    Updates.

---
+++ web/src/lib/components/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { page } from '$app/stores';
+    import { Navbar, NavBrand, NavUl, NavLi, DarkMode } from 'flowbite-svelte';
+    $: activeUrl = $page.url.pathname;
+
+  </script>
+
+  <Navbar rounded color="form" fluid>
+    <NavBrand href="/">
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Tangle</span>
+      <NavUl>
+        <NavLi href="/">Home</NavLi>
+        <DarkMode class="justify-end" />
+      </NavUl>
+    </NavBrand>
+  </Navbar>
